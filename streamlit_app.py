2af84bc4ff228dd6069d3545b376c7a0cae6a83e
panrambo <124465864+panrambo@users.noreply.github.com>
2023-02-04 21:14:53 +0100

    Update streamlit_app.py
    Added another three items an

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('Hey ho, 2 cats singing')
 
 streamlit.header('Oh my god')
 streamlit.text('Kale has just eated a dog')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
