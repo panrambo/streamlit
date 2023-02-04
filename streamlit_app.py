67262bd39cbfe4880a510f5febc3eb0301d93410
panrambo <124465864+panrambo@users.noreply.github.com>
2023-02-04 21:27:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ my_fruit_list = my_fruit_list.set_index(
 )
 
 
-fruits_to_show = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])                             
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])                            
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 streamlit.title('Hey ho, 2 cats singing')
 
