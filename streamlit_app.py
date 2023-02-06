9e8dba3fea8436ffff7c4af5f4d514ab06f2d220
panrambo <124465864+panrambo@users.noreply.github.com>
2023-02-06 19:50:25 +0100

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
                                 
-                                
+import requests
+fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
