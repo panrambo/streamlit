cdd1fecdb2256cbb959ebb9a511d2e9a470b4404
panrambo <124465864+panrambo@users.noreply.github.com>
2023-02-06 21:52:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-my_data_rows = my_cur.fetchALL()
+my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
